e2b0288e040ff74371a372c2b42575bcbeeee06f
Shuyu Li <shuyu3@student.unimelb.edu.au>
2021-05-16 11:08:34 +0800

    update couchDB setting

--- Twitter Harvester/couchDB_setting.py
+++ Twitter Harvester/couchDB_setting.py
@@ -1,14 +1,21 @@
 import couchdb
 import json
+import requests
 
 
-COUCHDB_SERVER='http://admin:admin@172.26.132.158:49186/'
+COUCHDB_SERVER='http://admin:admin@172.26.128.214:5984/'
 DBNAME = 'tiny_sample'
 couch = couchdb.Server(COUCHDB_SERVER)
 db = couch[DBNAME]
 
-
-
+full_text_search_payload = {
+  "_id": "_design/textsearch/text",
+  "indexes": {
+    "text": {
+      "index": "function (doc) { index(\"default\", doc._id); index ('text', doc.text, {store: true}); index ('language', doc.lang, {facet: true, store: true});}"
+    }
+  }
+}
 
 def upload2couchDB(row):
     if type(row) == dict:
@@ -16,3 +23,43 @@ def upload2couchDB(row):
     else:
         data = json.loads(row)
         db.save(data)
+
+
+def get_view(design_doc, view_name, group_level = '0', reduce = 'true'):
+    return db.view(design_doc + '/' + view_name, group_level = group_level, reduce = reduce)
+
+def post_index(payload):
+    url = COUCHDB_SERVER + DBNAME + '/_index'
+    headers = {"Content-Type": "application/json"}
+    if type(payload) != dict:
+        raise TypeError('data has to be a dict')
+    return json.loads(requests.post(
+        url, 
+        data = json.dumps(payload), 
+        headers = headers)
+        .content.decode("utf-8"))
+
+def get_index():
+    return db.index()
+
+def put_text_search(payload = full_text_search_payload, design_doc = 'textsearch'):
+    url = COUCHDB_SERVER + DBNAME + '/_design' + '/' + design_doc
+    headers = {"Content-Type": "application/json"}
+    if type(payload) != dict:
+        raise TypeError('data has to be a dict')
+    return json.loads(requests.put(
+        url, 
+        data = json.dumps(payload), 
+        headers = headers)
+        .content.decode("utf-8"))
+
+def search_query(query, design_doc = 'textsearch', index_name = 'text'):
+    url = COUCHDB_SERVER + DBNAME + '/_design/' + 'design_doc' + '/_search/' + index_name
+    headers = {"Content-Type": "application/json"}
+    if type(query) != dict:
+        raise TypeError('data has to be a dict')
+    return json.loads(requests.post(
+        url, 
+        data = json.dumps(query), 
+        headers = headers)
+        .content.decode("utf-8"))
