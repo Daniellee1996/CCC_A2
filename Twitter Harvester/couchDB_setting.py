5ef1944a74f274cf49f0f597673f30e6df1a4327
jiajun0024 <jiajunl9@student.unimelb.edu.au>
2021-05-17 12:00:52 +1000

    add map reduce in couchdb search

--- Twitter Harvester/couchDB_setting.py
+++ Twitter Harvester/couchDB_setting.py
@@ -63,3 +63,22 @@ def search_query(query, design_doc = 'textsearch', index_name = 'text'):
         data = json.dumps(query), 
         headers = headers)
         .content.decode("utf-8"))
+
+def map_reduce_fuc():
+    url = 'http://admin:admin@172.26.128.214:5984/' + DBNAME + '/_design/textsearch/_search/text'
+    map_func = "function(doc) {{\
+            var re = /covid|coronavirus|corona|cov19|corona|virus|cov |isolation|lockdown/;\
+            if(re.test(doc.text.toLowerCase()))\
+            emit([doc.place.name],1);\
+        }\
+    }"
+    reduce_func = " function (key, values, rereduce) { return sum(values); }"
+    data = {"views": {"my_filter":
+                          {"map": map_func,
+                           "reduce": reduce_func
+                           },
+                      }}
+
+    r = requests.put(url, data=json.dumps(data), headers=headers)
+    for item in db.view('new_doc/my_filter', group_level='2', reduce='true'):
+        print(item.key, item.id, item.value)
