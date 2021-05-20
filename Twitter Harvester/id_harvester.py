00c6bdf50b0d508f39dd080d31e867cabe727404
Shuyu Li <shuyu3@student.unimelb.edu.au>
2021-05-20 23:36:05 +0800

    bug fixed

--- Twitter Harvester/id_harvester.py
+++ Twitter Harvester/id_harvester.py
@@ -2,16 +2,19 @@ import tweepy
 from tweepy import OAuthHandler
 import pandas as pd
 import datetime
+import time
 from config import TwitterCredentails # your keys, format list of dicts
 
 KEY_INDEX = 0
 MAX_ID = None
 N_IDS = 0
+TIME = [None, None, None]
 
 
 def load_api():
     ''' Function that loads the twitter API after authorizing the user. '''
     """ todo: switch keys when window limit is reached """
+    global KEY_INDEX, TIME
     consumer_key = TwitterCredentails[KEY_INDEX].get('API_KEY')
     consumer_secret = TwitterCredentails[KEY_INDEX].get('API_SECRET_KEY')
     access_token = TwitterCredentails[KEY_INDEX].get('ACCESS_TOKEN')
@@ -19,6 +22,7 @@ def load_api():
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     # load the twitter API via tweepy
+    TIME[KEY_INDEX] = time.time()
     return tweepy.API(auth)
 
 
@@ -36,10 +40,10 @@ def get_AU_place_id(api):
 
 def tweet_write(user_id, outfile = 'cursor_ids.txt'):
     with open(outfile, 'a') as f:
-        f.write(user_id)
+        f.write(user_id + '\n')
 
 def main():
-    global KEY_INDEX, MAX_ID, N_IDS
+    global KEY_INDEX, MAX_ID, N_IDS, TIME
     api = load_api()
     # api.verify_credentials()
     AU_place_id = get_AU_place_id(api)
@@ -61,12 +65,25 @@ def main():
             MAX_ID = id_uids[-1][0] - 1
             N_IDS += len(id_uids)
             print(N_IDS, 'ids harvested...')
-            
+
         except tweepy.error.TweepError as e:
-            print(e)
-            switch_keys()
-            print('key switched')
-            main()
+            if None in TIME:
+                print(e)
+                switch_keys()
+                print('key switched to', KEY_INDEX)
+                main()
+            elif time.time() - min(TIME) < 60 * 15:
+                time.sleep(15 * 60 - (time.time() - min(TIME)) + 10)
+                print(e)
+                switch_keys()
+                print('key switched to', KEY_INDEX)
+                main()
+            else:
+                print(e)
+                switch_keys()
+                print('key switched to', KEY_INDEX)
+                main()
+                
         except StopIteration:
             print(new_tweets)
             pass
