33b327cd559c94401d09ef1484b84aafdb670b65
jiajun0024 <jiajunl9@student.unimelb.edu.au>
2021-05-22 01:25:10 +1000

    change the config

--- Twitter Harvester/timeline_test.py
+++ Twitter Harvester/timeline_test.py
@@ -12,19 +12,36 @@ import couchdb
 import json
 import os
 from pathlib import Path
-from config import TwitterCredentails # your keys, format list of dicts
+#from config import TwitterCredentails # your keys, format list of dicts
 from couchDB_setting import *
 from sentimental_score_calculator import SentiScoreCalculator
 
 KEY_INDEX = 0
 ID_INDEX = 0
 N_IDS = 0
-
+TwitterCredentails = [
+    # shuyu li
+    {
+        'API_KEY':'ZRe0EbBzCbWYvo8fcI7R2Cmah',
+        'API_SECRET_KEY':'JpofwKllwrSUjISwuvihaFu1Lb4iUWlTlwGssPEUqswT4woyhf',
+        'ACCESS_TOKEN': '1131912292054863872-An1go1FZ4dRRAopydTKVoNhEM1THW9',
+        'ACCESS_TOEKN_SECRET':'fvarVeChN9jBHqdMbeD42yBs1XHUUGI9KK5ctRBoxGDg6',
+        'BEARER_TOKEN':'AAAAAAAAAAAAAAAAAAAAAAK9OwEAAAAA%2FgK%2ByLXx%2FQXcmcA9YVl4z3Bdx1w%3D8Xu5hQ2gLyqNOemnbVV5s2PoDFdnZvQhRTtd0XFXk2iCxPMcJl'
+    },
+    # kd
+    {
+        'API_KEY':'aY9489Yex1OjIOaLrAZ0fDils',
+        'API_SECRET_KEY':'4iogUNgX71wcz9l85Z4BNaW4cLg8xpdNcuosQjrUkvtF2tq5aE',
+        'ACCESS_TOKEN': '1289252085033127936-ptoDhOwwYThSPW6qgQuihhV2NI8EWA',
+        'ACCESS_TOEKN_SECRET':'0DwAasvL9Ob9VX4iZg681AZCD07bq1N7e7ZehTfqJS0mI',
+        'BEARER_TOKEN':'AAAAAAAAAAAAAAAAAAAAAEJaPAEAAAAA37Fvd1A5M8pU7%2FfTFB2kbxRRpzY%3DYjvYAB0XIGQDk8J5oF2GvkNavfwFgyYSdnCasjocDa46HWym3u'
+    }
+]
 #CouchDB authentication
-# COUCHDB_SERVER='http://admin:password@172.26.132.199:5984/'
-# DBNAME = 'test'
-# couch = couchdb.Server(COUCHDB_SERVER)
-# db = couch[DBNAME]
+COUCHDB_SERVER='http://admin:admin@172.26.128.214:5984/'
+DBNAME = 'test'
+couch = couchdb.Server(COUCHDB_SERVER)
+db = couch[DBNAME]
 
 #Access tokens
 
