9dd8515eab4d174452c01bc655ce7ed22ff7162f
jiajun0024 <jiajunl9@student.unimelb.edu.au>
2021-05-25 00:33:15 +1000

    test a.py

--- frontend/a.py
+++ frontend/a.py
@@ -15,40 +15,10 @@ def homepage():
     return render_template('index.html')
     
 COUCHDB_SERVER='http://admin:admin@172.26.128.214:5984/'
-DBNAME = 'tiny_sample'
+DBNAME = 'twitter'
 couch = couchdb.Server(COUCHDB_SERVER)
 db = couch[DBNAME]
-def map_reduce_fuc():
-    url = 'http://admin:admin@172.26.128.214:5984/' + DBNAME + '/_design/filter_city'
-    map_func = "function(doc) {{\
-            var re = /covid|coronavirus|corona|cov19|corona|virus|cov |isolation|lockdown/;\
-            if(re.test(doc.text.toLowerCase()))\
-            emit([doc.place.name],1);\
-        }\
-    }"
-    reduce_func = " function (key, values, rereduce) { return sum(values); }"
-    data = {"views": {"my_filter":
-                          {"map": map_func,
-                           "reduce": reduce_func
-                           },
-                      }}
-    headers = {"Content-Type": "application/json"}
-    r = requests.put(url, data=json.dumps(data), headers=headers)
-    print(r.content)
-    list_city = {}
-    key = []
-    value = []
-    for item in db.view('new_doc/my_filter', group_level='2', reduce='true'):
-        #print(item.key, item.id, item.value)
-        key.append(item.key[0])
-        value.append(item.value)
 
-    list_city['city'] = key
-    list_city['value'] = value
-
-    j=json.dumps(list_city)
-    print(j)
-    return j
 
 @app.route('/simple_page')
 def simple_page():
@@ -60,7 +30,17 @@ def shortcodes():
 
 @app.route('/view_city', methods = ['GET', 'POST'])
 def get_view():
-    return map_reduce_fuc()
+    list_covid_time = {}
+    for item in db.view('covid_time/filter_covid_time', group_level='2', reduce='true'):
+        date = item.key.split("-")
+        print(date)
+        time = date[0] + "-" + date[1]
+        if time not in list_covid_time:
+            list_covid_time[time] = item.value
+        else:
+            list_covid_time[time] += item.value
+    # print(list_covid_time)
+    return list_covid_time
 
 @app.route('/refresh_count')
 def refresh_count():
